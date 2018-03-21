e4eac15a7627a1254868f5624fa90bac345dc4c8
Kim Foukaris <kimfoukaris@outlook.com>
2018-03-21 15:30:28 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -30,8 +30,8 @@ class InteractiveRecord
   end
 
   def col_names_for_insert
-    self.class.column_names.delete_if{|col| col=="id"}.join{", "}
-    binding.pry
+    self.class.column_names
+    #binding.pry
   end
 
 end
